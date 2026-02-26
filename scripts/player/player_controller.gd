4aaaf44ead173dace663e65deb732ed6633fae31
Sam <46286293+fireproofpenguin@users.noreply.github.com>
2026-02-26 14:47:03 +0000

    Basic player movement

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -1,31 +1,44 @@
 class_name PlayerController extends CharacterBody3D
 
-
+@export_category("Player settings")
+@export var acceleration: float = 2.0
+@export var deceleration: float = 1.0
+@export var speed: float = 5.0
 #const SPEED = 5.0
 #const JUMP_VELOCITY = 4.5
-#
-#
-#func _physics_process(delta):
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+
+var _input_dir: Vector2 = Vector2.ZERO
+var _movement_velocity: Vector3 = Vector3.ZERO
+
+func _unhandled_key_input(event):
+	if event.is_action("debug_quit"):
+		get_tree().quit()
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 #
 	## Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		#velocity.y = JUMP_VELOCITY
 #
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-#
-	#move_and_slide()
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	_input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var current_velocity = Vector2(_movement_velocity.x, _movement_velocity.z)
+	var direction = (transform.basis * Vector3(_input_dir.x, 0, _input_dir.y)).normalized()
+	
+	if direction:
+		current_velocity = lerp(current_velocity, Vector2(direction.x, direction.z) * speed, acceleration)
+	else:
+		current_velocity = current_velocity.move_toward(Vector2.ZERO, deceleration)
+	
+	_movement_velocity = Vector3(current_velocity.x, velocity.y, current_velocity.y)
+	
+	velocity = _movement_velocity
+	
+	move_and_slide()
 
 func update_rotation(rotation_input: Vector3) -> void:
 	global_transform.basis = Basis.from_euler(rotation_input)
